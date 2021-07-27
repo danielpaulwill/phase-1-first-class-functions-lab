129fb766634a64f7ba8a2ddacf3f17efff14fd0e
danielpaulwill <86324206+danielpaulwill@users.noreply.github.com>
2021-07-26 22:22:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
-//const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
 
-/*
-function returnFirstTwoDrivers() {
-    return function () {
-        console.log(drivers[0, 1]);
-    }
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
 }
 
-
-const returnFirstTwoDrivers = function(driver1 = "Sally", driver2 = "Bob", driver3 = "Freddy", driver4 = "Claudia") {
-    console.log(drivers.slice(0, 1));
-    return drivers.slice(0, 1);
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(2, 4);
 }
 
+const selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()]
 
 
-
-function returnFirstTwoDrivers() {
-    const drivers = function (driver1, driver2) {
-    return driver1 + driver2;
-    }
-}
-*/
-
-const returnFirstTwoDrivers = function (d1, d2, d3, d4) {
-    return function.slice(0, 1);
-}
-
-
-const drivers = function (num1, num2) {
-    return num1 + num2;
-}
-
-console.log(drivers(2, 5));
+selectingDrivers[0];
