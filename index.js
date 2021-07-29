317e440a7ca765ca0b7fa5a2b1fc407e9bc9582b
Daniel Williams <danielpaulwill@gmail.com>
2021-07-28 20:46:49 -0400

    commit tree 38a1b3d2b39baa6fff10980bf0df78f1266d7c81

--- index.js
+++ index.js
@@ -8,7 +8,37 @@ const returnLastTwoDrivers = function (drivers) {
     return drivers.slice(2, 4);
 }
 
-const selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()]
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num1) {
+    return function (num2) {
+        return num1 * num2;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
 
 
-selectingDrivers[0];
+function selectDifferentDrivers(arrayOfDrivers, driversFunction) {
+    if (driversFunction === returnLastTwoDrivers)
+        return returnLastTwoDrivers(arrayOfDrivers);
+        else if (driversFunction === returnFirstTwoDrivers)
+            return returnFirstTwoDrivers(arrayOfDrivers);
+        else return "No drivers for you!";
+}
+
+/*
+function selectDifferentDrivers(drivers, function) {
+    if function = returnFirstTwoDrivers {
+        return returnFirstTwoDrivers(drivers);
+    }   else if function = returnLastTwoDrivers {
+        return returnLastTwoDrivers(drivers);
+    }
+}
+
+*/ 
+
+
+console.log(returnLastTwoDrivers(drivers));
